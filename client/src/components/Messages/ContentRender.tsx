5646c53d75032f5bab2a5bf3cfde12fb8a1428b3
deli <jay.yjx@tearline.xyz>
2025-05-20 16:06:46 +0800

    fix: fix

--- client/src/components/Messages/ContentRender.tsx
+++ client/src/components/Messages/ContentRender.tsx
@@ -177,75 +177,75 @@ const ContentRender = memo(
                 attachments={msg.attachments}
                 isCreatedByUser={msg.isCreatedByUser}
                 conversationId={conversation?.conversationId}
-                // content={msg.content as Array<TMessageContentParts | undefined>}
-                content={[
-                  {
-                    type: 'text',
-                    text: '',
-                    tool_call_ids: ['call_jRzVcHcFrp7BqsDGqRRw64EK'],
-                  },
-                  {
-                    type: 'tool_call',
-                    tool_call: {
-                      id: 'call_jRzVcHcFrp7BqsDGqRRw64EK',
-                      name: 'search_tools_mcp_everything',
-                      args: '{"query":"bnb current price exchange rate"}',
-                      type: 'tool_call',
-                      progress: 1,
-                      output: `{
-                  "msg": "deposit transaction has been generated. Remind the use to adjust the parameters and sign the transaction on the interactive window below the chat box",
-                  "data": {
-                    "action": "cetus_deposit_v1",
-                    "data": {
-                      "pool_address": "0xb8d7d9e66a60c239e7a60110efcf8de6c705580ed924d0dde141f4a0e2c90105",
-                      "amount_a": 1000,
-                      "amount_b": 0,
-                      "tick_upper": 83069,
-                      "tick_lower": 82692
-                    }
-                  }
-                }`,
-                    },
-                  },
-                  {
-                    type: 'text',
-                    text: '',
-                    tool_call_ids: ['call_FMppNmgDmERBPIiCe5uTlusI'],
-                  },
-                  {
-                    type: 'tool_call',
-                    tool_call: {
-                      id: 'call_FMppNmgDmERBPIiCe5uTlusI',
-                      name: 'invoke_tool_mcp_everything',
-                      args: '{"tool_name":"get_token_price_latest","tool_kw_args":{"symbol":"bnb"}}',
-                      type: 'tool_call',
-                      progress: 1,
-                      output:
-                        '{"symbol": "BNB", "price": 604.3974743523231, "volume_24h": 1559103077.0885866, "percent_change_7d": 0.48774501, "volume_change_24h": 2.5365}',
-                    },
-                  },
-                  {
-                    type: 'text',
-                    text: '',
-                    tool_call_ids: ['call_sc4EHlCsHjlTSkfDuVicKbtY'],
-                  },
-                  {
-                    type: 'tool_call',
-                    tool_call: {
-                      id: 'call_sc4EHlCsHjlTSkfDuVicKbtY',
-                      name: 'invoke_tool_mcp_everything',
-                      args: '{"tool_name":"token_swap","tool_kw_args":{"from_token":"USD","to_token":"BNB","amount":1000}}',
-                      type: 'tool_call',
-                      progress: 1,
-                      output:
-                        '{"msg": "The transaction has been generated, need to check and sign it. Remind the user to click on the interactive window below the chat box to complete the transaction", "data": {"acton": "okx_swap_v1", "data": [{"from_coin": "0x55d398326f99059ff775485246999027b3197955", "from_decimal": 18, "from_symbol": "USDT", "to_coin": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "to_decimal": 18, "to_symbol": "BNB", "from_amount": "1000.0", "to_amount": "0", "chain_name": "BNB Chain", "chain_id": 56}, {"from_coin": "0xdac17f958d2ee523a2206206994597c13d831ec7", "from_decimal": 6, "from_symbol": "USDT", "to_coin": "0xB8c77482e45F1F44dE1745F52C74426C631bDD52", "to_decimal": 18, "to_symbol": "BNB", "from_amount": "1000.0", "to_amount": "0", "chain_name": "Ethereum", "chain_id": 1}]}}',
-                    },
-                  },
-                  {
-                    type: 'text',
-                    text: '当前BNB（币安币）的价格是604.40 USD。使用1000 USD兑换BNB的交易已生成，您需要点击聊天框下方的互动窗口以完成交易。\n\n您将能够在以下两条链上执行交换：\n1. BNB Chain\n2. Ethereum\n\n请确保查看并签署交易。',
-                  },
-                ]}
+                content={msg.content as Array<TMessageContentParts | undefined>}
+                // content={[
+                //   {
+                //     type: 'text',
+                //     text: '',
+                //     tool_call_ids: ['call_jRzVcHcFrp7BqsDGqRRw64EK'],
+                //   },
+                //   {
+                //     type: 'tool_call',
+                //     tool_call: {
+                //       id: 'call_jRzVcHcFrp7BqsDGqRRw64EK',
+                //       name: 'search_tools_mcp_everything',
+                //       args: '{"query":"bnb current price exchange rate"}',
+                //       type: 'tool_call',
+                //       progress: 1,
+                //       output: `{
+                //   "msg": "deposit transaction has been generated. Remind the use to adjust the parameters and sign the transaction on the interactive window below the chat box",
+                //   "data": {
+                //     "action": "cetus_deposit_v1",
+                //     "data": {
+                //       "pool_address": "0xb8d7d9e66a60c239e7a60110efcf8de6c705580ed924d0dde141f4a0e2c90105",
+                //       "amount_a": 1000,
+                //       "amount_b": 0,
+                //       "tick_upper": 83069,
+                //       "tick_lower": 82692
+                //     }
+                //   }
+                // }`,
+                //     },
+                //   },
+                //   {
+                //     type: 'text',
+                //     text: '',
+                //     tool_call_ids: ['call_FMppNmgDmERBPIiCe5uTlusI'],
+                //   },
+                //   {
+                //     type: 'tool_call',
+                //     tool_call: {
+                //       id: 'call_FMppNmgDmERBPIiCe5uTlusI',
+                //       name: 'invoke_tool_mcp_everything',
+                //       args: '{"tool_name":"get_token_price_latest","tool_kw_args":{"symbol":"bnb"}}',
+                //       type: 'tool_call',
+                //       progress: 1,
+                //       output:
+                //         '{"symbol": "BNB", "price": 604.3974743523231, "volume_24h": 1559103077.0885866, "percent_change_7d": 0.48774501, "volume_change_24h": 2.5365}',
+                //     },
+                //   },
+                //   {
+                //     type: 'text',
+                //     text: '',
+                //     tool_call_ids: ['call_sc4EHlCsHjlTSkfDuVicKbtY'],
+                //   },
+                //   {
+                //     type: 'tool_call',
+                //     tool_call: {
+                //       id: 'call_sc4EHlCsHjlTSkfDuVicKbtY',
+                //       name: 'invoke_tool_mcp_everything',
+                //       args: '{"tool_name":"token_swap","tool_kw_args":{"from_token":"USD","to_token":"BNB","amount":1000}}',
+                //       type: 'tool_call',
+                //       progress: 1,
+                //       output:
+                //         '{"msg": "The transaction has been generated, need to check and sign it. Remind the user to click on the interactive window below the chat box to complete the transaction", "data": {"acton": "okx_swap_v1", "data": [{"from_coin": "0x55d398326f99059ff775485246999027b3197955", "from_decimal": 18, "from_symbol": "USDT", "to_coin": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "to_decimal": 18, "to_symbol": "BNB", "from_amount": "1000.0", "to_amount": "0", "chain_name": "BNB Chain", "chain_id": 56}, {"from_coin": "0xdac17f958d2ee523a2206206994597c13d831ec7", "from_decimal": 6, "from_symbol": "USDT", "to_coin": "0xB8c77482e45F1F44dE1745F52C74426C631bDD52", "to_decimal": 18, "to_symbol": "BNB", "from_amount": "1000.0", "to_amount": "0", "chain_name": "Ethereum", "chain_id": 1}]}}',
+                //     },
+                //   },
+                //   {
+                //     type: 'text',
+                //     text: '当前BNB（币安币）的价格是604.40 USD。使用1000 USD兑换BNB的交易已生成，您需要点击聊天框下方的互动窗口以完成交易。\n\n您将能够在以下两条链上执行交换：\n1. BNB Chain\n2. Ethereum\n\n请确保查看并签署交易。',
+                //   },
+                // ]}
               />
             </div>
 
